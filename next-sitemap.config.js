c1c5b09c3f9c56136185dc45f81f70be9b1a9ccf
Kase390 <80099741+Kase390@users.noreply.github.com>
2023-11-17 01:08:30 +0900

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const siteMetadata = require("./project files/siteMetaData");
+
+
+module.exports = {
+  siteUrl: siteMetadata.siteUrl,
+  generateRobotsTxt: true,
+}
---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+const {withContentlayer} = require('next-contentlayer');
+
+const nextConfig = {};
+
+module.exports =  withContentlayer({ ...nextConfig });
+
+
